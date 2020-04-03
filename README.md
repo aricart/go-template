76801b74e3af6828bc2cd450ee9a61dbd7a305c5
Alberto Ricart <alberto@synadia.com>
2020-04-03 15:12:19 -0500

    initial

---
+++ README.md
@@ -0,0 +1,7 @@
+# go-template
+
+This is a simple template for a go project build
+
+## Usage
+
+`go-template`
---
+++ main_test.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func Test_SayHello(t *testing.T) {
+	require.Equal(t, "hello!", sayHello())
+}
